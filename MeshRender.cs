7610b778accf2be1fc665a70a738a07708b26f25
Rik_Rodel <rdel0002@hz.nl>
2018-11-16 13:27:27 +0100

    script to disable mesh renders in game on runtime

---
+++ MeshRender.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRender : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+    }
+}
