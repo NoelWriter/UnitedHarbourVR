430760a28cd016682e1f7e4dbd997cb2d1e58775
Jordi Schaaf <Scha0071@hz.nl>
2018-11-29 13:59:30 +0100

    Added the option to autogenerate meshcolliders on objects with the tag 'Building'

---
+++ Assets/Scripts/ColliderWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ColliderWindow : EditorWindow
+{
+    [MenuItem("Window/Edit Colliders")]
+    public static void ShowEditorWindow()
+    {
+        var window = GetWindow<ColliderWindow>();
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        var collider = new ColliderOptions();
+        if (GUILayout.Button("Generate colliders"))
+        {
+            collider.Generate();
+        }
+
+    }
+}
