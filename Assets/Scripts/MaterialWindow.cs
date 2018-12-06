d2c2da1e7d188abc6cf784bfe24fea469629aa64
Jordi Schaaf <Scha0071@hz.nl>
2018-12-06 15:02:29 +0100

    Working window to add materials to objects with a chosen tag

---
+++ Assets/Scripts/MaterialWindow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MaterialWindow : EditorWindow
+{
+    private string tagStr = "";
+    private Material selectedMaterial;
+
+    [MenuItem("Window/Edit Materials")]
+    public static void ShowEditorWindow()
+    {
+        var window = GetWindow<MaterialWindow>();
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Space(20);
+        tagStr = EditorGUILayout.TagField("Select tag:", tagStr);
+
+
+        if (tagStr != "")
+        {
+            GUILayout.Space(20);
+            selectedMaterial = EditorGUILayout.ObjectField("Select material:", selectedMaterial, typeof(Material), false) as Material;
+        }
+
+        var material = new MaterialOptions(tagStr);
+
+        if (selectedMaterial != null)
+        {
+            GUILayout.Space(20);
+            if (GUILayout.Button("Apply Material"))
+            {
+                material.ApplyMaterial(selectedMaterial);
+            }
+        }
+    }
+
+}
