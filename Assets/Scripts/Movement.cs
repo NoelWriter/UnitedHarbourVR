0b2458ee3c6263f31367be10f522fed8dd417ecf
RemcoKok <kok0047@hz.nl>
2018-12-09 13:31:20 +0100

    Created the boat prefab with a path

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+
+    public Transform[] target;
+    public float speed;
+    private int current;
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, target[current].position) > 10)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else
+        {
+            current = (current + 1) % target.Length;
+        }
+        steer();
+    }
+    private void steer()
+    {
+        Vector3 targetDir = target[current].position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float step = ((speed/40)/2) * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward , targetDir, step, 0.0f);
+
+
+        // Move our position a step closer to the target.
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
+
