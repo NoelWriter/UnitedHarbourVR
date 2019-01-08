577afb899a31df059ced9dfdbe4fb056874ee745
NoelWriter <schr0112@hz.nl>
2019-01-08 15:16:12 +0100

    Fixed truck and car path issues and optimized code

--- Assets/Scripts/MovementTruck.cs
+++ Assets/Scripts/MovementTruck.cs
@@ -10,44 +10,47 @@ public class MovementTruck : MonoBehaviour
     private int current;
     public Transform[] wheels;
     public Transform[] turningWheels;
+    private float distance;
 
     private void Update()
     {
+        //Updating variables
+        distance = Vector3.Distance(transform.position, target[current].position);
+
+        //Updating Wheelrotation
         rotateWheels();
         steer();
-        if (Vector3.Distance(transform.position, target[current].position) > 3)
-            {
-                Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
-                GetComponent<Rigidbody>().MovePosition(pos);
-            }
-            {
-                if (Vector3.Distance(transform.position, target[current].position) > 1)
-                {
-                    Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, (speed / 4) * Time.deltaTime);
-                    GetComponent<Rigidbody>().MovePosition(pos);
-                }
-                else
-                {
-                    current = (current + 1) % target.Length;
-                }
-            }
-        
-       
+
+        //Updating speed over distance
+        if (distance > 3)
+            MoveTowards(speed);
+        else if (distance > 1)
+            MoveTowards(speed / 4);
+        else
+        {
+            current = (current + 1) % target.Length;
+        }      
     }
+
+    private void MoveTowards(float moveSpeed)
+    {
+        Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, moveSpeed * Time.deltaTime);
+        GetComponent<Rigidbody>().MovePosition(pos);
+    }
+
     private void steer()
     {
+        // Calculate rotation to destination
         Vector3 targetDir = target[current].position - transform.position;
 
-        // The step size is equal to speed times frame time.
-        float step = (speed / 5) * Time.deltaTime;
-
+        // The step size is equal to speed / 5 times frame time.
+        float step = 0.5f * Time.deltaTime;
         Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
-
 
         // Move our position a step closer to the target.
         transform.rotation = Quaternion.LookRotation(newDir);
-
     }
+
     private void rotateWheels()
     {
         for (int i = 0; i < wheels.Length; i++)
